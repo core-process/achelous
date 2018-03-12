7a774a6024f6b946cd3696deacb650c11e1a09ed
Niklas Salmoukas <niklas@salmoukas.com>
2018-03-12 03:20:00 +0100

    try to send SIGUSR1 to child processes if we cannot send to primary process

--- common/queue/trigger.go
+++ common/queue/trigger.go
@@ -1,11 +1,15 @@
 package queue
 
 import (
+	"errors"
 	"io/ioutil"
+	"log"
 	"strconv"
 	"syscall"
 
 	"github.com/core-process/achelous/common/config"
+
+	ps "github.com/mitchellh/go-ps"
 )
 
 func Trigger() error {
@@ -24,7 +28,34 @@ func Trigger() error {
 	// send SIGUSR1 signal
 	err = syscall.Kill(int(pid), syscall.SIGUSR1)
 	if err != nil {
-		return err
+		log.Printf("failed to send SIGUSR1 to primary process: %v", err)
+
+		// retrieve list of all processes
+		log.Printf("trying to send SIGUSR1 to child processes...")
+		procs, err := ps.Processes()
+		if err != nil {
+			return err
+		}
+
+		// try to send signal to child processes
+		sentToChild := false
+
+		for _, proc := range procs {
+			// compare parent pid to primary process pid
+			if proc.PPid() == int(pid) {
+				// send SIGUSR1 to child process
+				err = syscall.Kill(proc.Pid(), syscall.SIGUSR1)
+				if err != nil {
+					log.Printf("failed to send SIGUSR1 to child process %d: %v", proc.Pid(), err)
+				} else {
+					sentToChild = true
+				}
+			}
+		}
+
+		if !sentToChild {
+			return errors.New("Failed to send SIGUSR1 to any process")
+		}
 	}
 
 	return nil
